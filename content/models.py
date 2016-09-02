cb4e46c2fca1d159a0447cc566a65cde22319c43
Nathaniel Sun <nathanielove@gmail.com>
2016-09-02 23:46:56 +0800

    updated schema details

--- content/models.py
+++ content/models.py
@@ -3,11 +3,14 @@ from django.db import models
 from section.models import Section
 from version.models import Version
 
-# Create your models here
+
 class Content(models.Model):
-	section = models.ForeignKey(Section)
-	version = models.ForeignKey(Version)
-	text = models.TextField()
-	
-	def __str__(self):
-		return self.section.title + ' - ' + self.version.id
+    section = models.ForeignKey(Section)
+    version = models.ForeignKey(Version)
+    text = models.TextField()
+
+    def __str__(self):
+        return "%s @ Version [%d]".format(self.section.title, self.id)  # changed: string.format() always preferred
+
+    class Meta(object):
+        unique_together = ('section', 'version')  # added unique together
--- version/models.py
+++ version/models.py
@@ -1,12 +1,11 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-from django.contrib.auth.models import User
 
-# Create your models here.
 class Version(models.Model):
-	name = models.TextField(max_length=45)
-	created_by = models.ForeignKey(User)
-	timestamp = models.DateTimeField(auto_now=True)
-	
-	def __str__(self):
-		return self.name + ' - ' + self.id
+    name = models.TextField()
+    created_by = models.ForeignKey(User)
+    timestamp = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return "%s [%d]".format(self.name, self.id)
