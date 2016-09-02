dee869e4ea282095a93fd6273601593da8c6426c
Nathaniel Sun <nathanielove@gmail.com>
2016-09-02 23:54:36 +0800

    fixed content model

--- content/models.py
+++ content/models.py
@@ -10,7 +10,7 @@ class Content(models.Model):
     text = models.TextField()
 
     def __str__(self):
-        return "%s @ Version [%d]".format(self.section.title, self.id)  # changed: string.format() always preferred
+        return "%s @ Version [%s]".format(self.section.title, self.version.name)  # string.format() always preferred
 
     class Meta(object):
         unique_together = ('section', 'version')  # added unique together
