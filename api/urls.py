75ec0de11022ad03f7fd59d840de161d34b38aa0
axawzh <wuevans@hotmail.com>
2016-09-03 23:13:19 +0800

    feature: added new urls

--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,8 @@ from django.conf.urls import url, include
 
 urlpatterns = [
     url(r'^book/', include('book.urls')),
-    url(r'^section/', include('section.urls'))
+    url(r'^section/', include('section.urls')),
+    url(r'^content/', include('content.urls')),
+    url(r'^version/', include('version.urls'))
+    
 ]
