36d62cb22da76edf517bb25f17f720ade3a1bf81
Nathaniel Sun <nathanielove@gmail.com>
2016-09-03 12:14:21 +0800

    improved section.admin.*

--- section/admin.py
+++ section/admin.py
@@ -6,11 +6,19 @@ from section.models import *
 @admin.register(Section)
 class SectionAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'book', 'has_children')
+    readonly_fields = ('book', 'has_children')
     search_fields = ('book', 'title')
     list_filter = ('book', 'has_children')
+
+    def has_add_permission(self, request):
+        return False
 
 
 @admin.register(Adjacency)
 class AdjacencyAdmin(admin.ModelAdmin):
     list_display = ('id', 'get_book', 'parent', 'child')
+    readonly_fields = ('parent', 'child')
     search_fields = ('parent', 'child')
+
+    def has_add_permission(self, request):
+        return False
