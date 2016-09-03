daaeea8f848933784b2734c69a3ad0f54e4673b1
Nathaniel Sun <nathanielove@gmail.com>
2016-09-03 12:26:22 +0800

    corrent auto_now to auto_now_add

--- version/models.py
+++ version/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Version(models.Model):
     name = models.TextField()
     created_by = models.ForeignKey(User)
-    timestamp = models.DateTimeField(auto_now=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return "%s [%d]".format(self.name, self.id)
