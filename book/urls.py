4027bb4a6442e55d614ebba2fb9612b6bb4a0b0f
Nathaniel Sun <nathanielove@gmail.com>
2016-09-04 14:52:16 +0800

    improve: refactored book views

--- book/urls.py
+++ book/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from book import views
 
 urlpatterns = [
-    url(r'^list/$', views.BookList.as_view()),
-    url(r'^detail/(?P<pk>[1-9]\d*)/$', views.BookDetail.as_view()),
-    url(r'^toc/(?P<pk>[1-9]\d*)/$', views.BookToc.as_view())
+    url(r'^list/$', views.BookViewSet.as_view({'get': 'list'})),
+    url(r'^detail/(?P<pk>[1-9]\d*)/$', views.BookViewSet.as_view({'get': 'retrieve'})),
+    url(r'^toc/(?P<pk>[1-9]\d*)/$', views.BookViewSet.as_view({'get': 'toc'}))
 ]
