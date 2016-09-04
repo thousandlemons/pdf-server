5b5ef04ee648c438db88cecc32abce4bfe06fbc8
axawzh <wuevans@hotmail.com>
2016-09-04 11:53:11 +0800

    Added skeleton for classes, to be completed

--- version/views.py
+++ version/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
 
 # Create your views here.
+import json
+
+from django.http import Http404
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from version.models import Version
+from version.serializers import VersionSerializer
+
+class VersionList(APIView):
+    def get(self, request, format=None):
+    
+
+class VersionDetail(APIView):
+    def get(self, request, pk, format=None):
+    
+
+class VersionCreate(APIView):
+    def post(self, request, format=None):
+    
+
+class VersionUpdate(APIView):
+    def post(self, request, pk, format=None):
+    
+
+class VersionDelete(APIView):
+    def post(self, request, pk, format=None):
