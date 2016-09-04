f0da3255823ca719948894e27ec863a7f7ddd782
axawzh <wuevans@hotmail.com>
2016-09-04 11:22:58 +0800

    Added skeleton for views, to be completed

--- content/views.py
+++ content/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
-
 # Create your views here.
+import json
+
+from django.http import Http404
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from content.models import Content
+from content.serializers import ContentSerializer
+
+class ContentImmediate(APIView):
+    def get(self, request, pk_section, pk_version= , format=None): # Default version key to be filled in.
+    
+
+class ContentAggregate(APIView):
+    def get(self, request, pk_section, pk_version= , format=None): # Default version key to be filled in.
+    
+
+class ContentPost(APIView):
+    def post(self, request, pk_section, pk_version, format=None):
+    
+    
+        
