a9a5807a846a869fcd78d9e548508bb4601487a1
axawzh <wuevans@hotmail.com>
2016-09-04 11:52:33 +0800

    Added urls for endpoints

--- version/urls.py
+++ version/urls.py
@@ -3,5 +3,9 @@ from django.conf.urls import url
 from version import views
 
 urlpatterns = [
-    
-    ]
+    url(r'list/$', views.VersionList.as_view()),
+    url(r'detail/(?P<pk>[1-9]+)/$', views.VersionDetail.as_view()),
+    url(r'create/$', views.VersionCreate.as_view()),
+    url(r'update/$', views.VersionUpdate.as_view()),
+    url(r'delete/$', views.VersionDelete.as_view()) 
+]
