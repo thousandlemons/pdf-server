181e53a33a90c2da65c5fe0f393de0eae05b9168
Nathaniel Sun <nathanielove@gmail.com>
2016-09-05 14:10:33 +0800

    fix: version url regex

--- version/urls.py
+++ version/urls.py
@@ -4,8 +4,9 @@ from version import views
 
 urlpatterns = [
     url(r'list/$', views.VersionViewSet.as_view({'get': 'list'})),
-    url(r'detail/(?P<pk>[1-9]+)/$', views.VersionViewSet.as_view({'get': 'retrieve'})),
+    url(r'detail/(?P<pk>[1-9]\d*)/$', views.VersionViewSet.as_view({'get': 'retrieve'})),
     url(r'create/$', views.VersionViewSet.as_view({'put': 'create'})),
-    url(r'update/(?P<pk>[1-9]+)/$', views.VersionViewSet.as_view({'post': 'update'})),
-    url(r'delete/(?P<pk>[1-9]+)/$', views.VersionViewSet.as_view({'delete': 'destroy'}))
+    url(r'update/(?P<pk>[1-9]\d*)/$', views.VersionViewSet.as_view({'post': 'update'})),
+    url(r'delete/(?P<pk>[1-9]\d*)/$', views.VersionViewSet.as_view({'delete': 'destroy'}))
 ]
+g
