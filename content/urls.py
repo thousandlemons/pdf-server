7a64d72cda8c5b40799d54a53ddb6c7725b959b3
Nathaniel Sun <nathanielove@gmail.com>
2016-09-07 18:57:30 +0800

    hotfix: content url regex

--- content/urls.py
+++ content/urls.py
@@ -6,10 +6,10 @@ urlpatterns = [
     # Version key is optional for Immediate Text and Aggregate Text,
     # but required for Post.
     url(r'immediate/(?P<section_pk>[1-9]\d*)/$', views.ContentViewSet.as_view({'get': 'immediate'})),
-    url(r'immediate/(?P<section_pk>[1-9]\d*)/(?P<version_pk>[1-9]+)/$',
+    url(r'immediate/(?P<section_pk>[1-9]\d*)/(?P<version_pk>[1-9]\d*)/$',
         views.ContentViewSet.as_view({'get': 'immediate'})),
     url(r'aggregate/(?P<section_pk>[1-9]\d*)/$', views.ContentViewSet.as_view({'get': 'aggregate'})),
-    url(r'aggregate/(?P<section_pk>[1-9]\d*)/(?P<version_pk>[1-9]+)/$',
+    url(r'aggregate/(?P<section_pk>[1-9]\d*)/(?P<version_pk>[1-9]\d*)/$',
         views.ContentViewSet.as_view({'get': 'aggregate'})),
-    url(r'post/(?P<section_pk>[1-9]\d*)/(?P<version_pk>[1-9]+)/$', views.ContentViewSet.as_view({'post': 'post'}))
+    url(r'post/(?P<section_pk>[1-9]\d*)/(?P<version_pk>[1-9]\d*)/$', views.ContentViewSet.as_view({'post': 'post'}))
 ]
