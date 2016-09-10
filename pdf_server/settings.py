4ca6efcaf492f8416678fefa33534be02c640c70
Nathaniel Sun <nathanielove@gmail.com>
2016-09-10 21:25:50 +0800

    fix: history

--- pdf_server/settings.py
+++ pdf_server/settings.py
@@ -143,3 +143,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+
+PROJECT_ROOT = os.path.normpath(os.path.dirname(__file__))
+STATIC_ROOT = os.path.abspath('/var/www/pdf-server/static')
